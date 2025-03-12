d1ac85ea98c995747e0af9be8aa6bca2149ad541
Tonymateo320 <tonymateo320@gmail.com>
2025-03-11 20:28:08 -0400

    Este es el archivo de configuracion o sea donde estan las contantes para que se necesita para el juego. Es una forma mas organizada para cuando se quiera modificar cualquiera cosas o sea ejemplo un color y que ese color lo este usando varias funcion, solo ahi que modifcarlo en este archivo luego los cambios se van a efectuar en todas las funciones y archivo pertinentes

---
+++ config.py
@@ -0,0 +1,46 @@
+# Constantes del juego (Tony Mateo 23-eisn-2-044)
+ANCHO, ALTO = 800, 600
+FPS = 60
+
+# Colores (Tony Mateo 23-eisn-2-044)
+BLANCO = (255, 255, 255)
+NEGRO = (0, 0, 0)
+ROJO = (255, 0, 0)
+VERDE = (0, 255, 0)
+AZUL = (0, 0, 255)
+GRIS = (128, 128, 128)
+MARRON = (139, 69, 19)
+AMARILLO=(255,255,0)
+
+# Tamaño de celda para el algoritmo A* (Tony Mateo 23-eisn-2-044)
+TAMANO_CELDA = 20
+
+# Configuración del jugador (Tony Mateo 23-eisn-2-044)
+JUGADOR_ANCHO = 30
+JUGADOR_ALTO = 30
+JUGADOR_VELOCIDAD = 5
+JUGADOR_VIDA = 100
+COOLDOWN_DISPARO = 200
+
+# Configuración de NPC (Tony Mateo 23-eisn-2-044)
+NPC_ANCHO = 30
+NPC_ALTO = 30
+NPC_VELOCIDAD = 2
+NPC_VIDA = 60
+NPC_COOLDOWN_DISPARO = 1000
+NPC_DISTANCIA_VISION = 200
+NPC_RECALCULO_CAMINO = 500
+NPC_TIEMPO_VAGAR = 3000
+
+# Configuración de balas (Tony Mateo 23-eisn-2-044)
+BALA_RADIO = 5
+BALA_VELOCIDAD = 10
+BALA_DANO_JUGADOR = 10
+BALA_DANO_NPC = 25
+
+# Información de niveles (Tony Mateo 23-eisn-2-044)
+MAX_NIVELES = 10
+
+  # Modo depuración (Tony Mateo 23-eisn-2-044)  
+DEBUG_MODE = True
+DEBUG_COLOR = (255, 0, 0)  
